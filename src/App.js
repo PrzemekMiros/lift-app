b233b457f9a22d9dfcf9d3de7c6daf3752d286d1
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-01-07 19:44:27 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <NavigationContainer>
       <StatusBar style="light" />
       <AppNavigator />
-    </SafeAreaView>
+    </NavigationContainer>
   );
 }
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,7 +1,7 @@
 const colors = {
   background: '#332e45',
   primary: '#54b286',
-  accent: '#54b286',
+  accent: '#fb8800',
   error: '#8a0101',
   text: '#f2f0f5',
   muted: '#c9c4d4',
